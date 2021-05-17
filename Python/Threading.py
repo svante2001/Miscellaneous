970f51a45b545b64463fe1c04947b10042d3eb55
Svante Geisshirt <svante2001@gmail.com>
2021-05-17 21:37:07 +0200

    Threads

--- Python/Threading.py
+++ Python/Threading.py
@@ -1,6 +1,5 @@
 import threading
 
-
 def square(numbers):
     for n in numbers:
         print('Square of %d is %d' % (n, n*n))
@@ -9,7 +8,7 @@ def cube(numbers):
     for n in numbers:
         print('Cube of %d is %d' % (n, n*n*n))
 
-arr = [2,4,6,8,10]
+arr = [1,2,3,4,5]
 
 t1= threading.Thread(target=square, args=(arr,))
 t2= threading.Thread(target=cube, args=(arr,))
