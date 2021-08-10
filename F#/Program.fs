13e0f8bcc363ba0f9ec26c827280e39b02fb7b0d
Svante Geisshirt <svante2001@gmail.com>
2021-08-10 16:34:00 +0200

    Primenumber finder

---
+++ F#/Program.fs
@@ -0,0 +1,14 @@
+﻿#light
+open System
+
+let mutable divisable = false
+let mutable j = 2
+
+for i = 2 to 100 do
+    j <- 2
+    while j < i do
+        if i % j = 0 then divisable <- true
+        j <- j + 1
+
+    if divisable = false then Console.WriteLine(i)
+    divisable <- false
