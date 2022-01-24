dbe5366e828987cb311253dbd4930114c849c280
svante2001 <svante2001@gmail.com>
2022-01-24 14:01:51 +0100

    push to stack

--- C/stack.c
+++ C/stack.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct element {
     int value;
@@ -9,16 +10,30 @@ struct stack {
     struct element *top;
 };
 
-// TODO: 
+void push (struct stack *stack, int v) {
+    struct element *e;
+    e = (struct element*)malloc(sizeof(struct element));
 
+    if (stack->top == NULL) {
+        stack->top = e;
+        e->value = v;
+        e->under = NULL;
+    } else {
+        e->under = stack->top;
+        stack->top = e;
+        e->value = v;
+    }
+}
 
 void printStack (struct stack *stack) {
     struct element *i;
     i = stack->top;
+    printf("Stack:\n");
     while (i != NULL) {
         printf("%d ", i->value);
         i = i->under;
     }
+    printf("\n");
 }
 
 int main() {
@@ -26,8 +41,8 @@ int main() {
     stack = (struct stack*)malloc(sizeof(struct stack));
     stack->top = NULL;
 
-    // push(stack, 1);
-    // push(stack, 2);
-
+    push(stack, 2);
+    push(stack, 4);
+    push(stack, 16);
     printStack(stack);
 }
