97a2f1675273a7b928c2635a8737b5301c556be8
svante2001 <svante2001@gmail.com>
2022-01-24 18:23:11 +0100

    pop and peek

--- C/stack.c
+++ C/stack.c
@@ -10,25 +10,35 @@ struct stack {
     struct element *top;
 };
 
-void push (struct stack *stack, int v) {
+void push (struct stack *s, int v) {
     struct element *e;
     e = (struct element*)malloc(sizeof(struct element));
 
-    if (stack->top == NULL) {
-        stack->top = e;
+    if (s->top == NULL) {
+        s->top = e;
         e->value = v;
         e->under = NULL;
     } else {
-        e->under = stack->top;
-        stack->top = e;
+        e->under = s->top;
+        s->top = e;
         e->value = v;
     }
+}
+
+int pop (struct stack *s) {
+    int topElement = s->top->value;
+    s->top = s->top->under;
+    return topElement;
+}
+
+int peek (struct stack *s) {
+    return s->top->value;
 }
 
 void printStack (struct stack *stack) {
     struct element *i;
     i = stack->top;
-    printf("Stack:\n");
+    printf("\nStack:\n");
     while (i != NULL) {
         printf("%d ", i->value);
         i = i->under;
@@ -45,4 +55,10 @@ int main() {
     push(stack, 4);
     push(stack, 16);
     printStack(stack);
+
+    printf("\nPop stack: %i\n", pop(stack));
+
+    printStack(stack);
+    
+    printf("\nPeek stack: %i\n", peek(stack));
 }
